2a808ad75b69de7a3697af3f8da23cef312623ef
Flo Wong <florencewong1020@gmail.com>
2017-12-21 12:17:45 -0800

    fix css

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ var gulp         = require("gulp"),
     sass         = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
     hash         = require("gulp-hash"),
-    del          = require("del")
+    del          = require("del");
 
 // Compile SCSS files to CSS
 gulp.task("scss", function () {
 
     //Delete our old css files
-    del(["static/css/**/*"])
+    // del(["static/css/**/*"])
 
     //compile hashed css files
-    gulp.src("src/scss/**/*.scss")
+    gulp.src("static/scss/**/*.scss")
         .pipe(sass({
-            outputStyle : "compressed"
+            outputStyle : "compact" //nested
         }))
         .pipe(autoprefixer({
             browsers : ["last 20 versions"]
@@ -23,28 +23,32 @@ gulp.task("scss", function () {
         .pipe(gulp.dest("static/css"))
 })
 
-// Hash images
-gulp.task("images", function () {
-    del(["static/images/**/*"])
-    gulp.src("src/images/**/*")
-        .pipe(hash())
-        .pipe(gulp.dest("static/images"))
-})
-
-// Hash javascript
-gulp.task("js", function () {
-    del(["static/js/**/*"])
-    gulp.src("src/js/**/*")
-        .pipe(hash())
-        .pipe(gulp.dest("static/js"))
-})
+// // Hash images
+// gulp.task("images", function () {
+//     del(["static/images/**/*"])
+//     gulp.src("src/images/**/*")
+//         .pipe(hash())
+//         .pipe(gulp.dest("static/images"))
+// })
+//
+// // Hash javascript
+// gulp.task("js", function () {
+//     del(["static/js/**/*"])
+//     gulp.src("src/js/**/*")
+//         .pipe(hash())
+//         .pipe(gulp.dest("static/js"))
+// })
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "images", "js"], function () {
-    gulp.watch("src/scss/**/*", ["scss"])
-    gulp.watch("src/images/**/*", ["images"])
-    gulp.watch("src/js/**/*", ["js"])
+gulp.task("watch", ["scss"], function () {
+    gulp.watch("static/scss/**/*.scss", ["scss"]);
 })
 
+// gulp.task("watch", ["scss", "images", "js"], function () {
+//     gulp.watch("src/scss/**/*", ["scss"])
+//     gulp.watch("src/images/**/*", ["images"])
+//     gulp.watch("src/js/**/*", ["js"])
+// })
+
 // Set watch as default task
-gulp.task("default", ["watch"])
+gulp.task("default", ["watch"]);
