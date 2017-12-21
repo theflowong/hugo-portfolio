66c13dbda6d0dbcb209e8cc8930aa854e690132d
Flo Wong <florencewong1020@gmail.com>
2017-12-21 11:33:41 -0800

    add files

--- content/posts/my-first-post.md
+++ content/posts/my-first-post.md
@@ -1,8 +1,8 @@
 ---
-title: "Aliquam"
+title: "Surreal Paintings"
 description: "Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus."
 slug: "aliquam"
-image: pic07.jpg
+image: ../oberon-bay.jpg
 keywords: ""
 categories:
     - ""
@@ -11,4 +11,10 @@ date: 2017-10-31T22:42:51-05:00
 draft: false
 ---
 
-Hello World!
+These are a random collection of oil paintings and studies that I've done in the past, under the supervisor David Lefkowitz. Check him out, he's great!
+
+In retrospect, a common thread to these paintings was their element of magical surrealism depicting fantastical dream-like landscapes.
+
+I've always aspired to portray my dreams, so my paintings might be reflecting that desire subonsciously.
+
+<img src="/img/oil/selfportrait_2.jpg" alt="Example image" style="width: 100%;" />
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+// Require dependencies
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    hash         = require("gulp-hash"),
+    del          = require("del")
+
+// Compile SCSS files to CSS
+gulp.task("scss", function () {
+
+    //Delete our old css files
+    del(["static/css/**/*"])
+
+    //compile hashed css files
+    gulp.src("src/scss/**/*.scss")
+        .pipe(sass({
+            outputStyle : "compressed"
+        }))
+        .pipe(autoprefixer({
+            browsers : ["last 20 versions"]
+        }))
+        .pipe(hash())
+        .pipe(gulp.dest("static/css"))
+})
+
+// Hash images
+gulp.task("images", function () {
+    del(["static/images/**/*"])
+    gulp.src("src/images/**/*")
+        .pipe(hash())
+        .pipe(gulp.dest("static/images"))
+})
+
+// Hash javascript
+gulp.task("js", function () {
+    del(["static/js/**/*"])
+    gulp.src("src/js/**/*")
+        .pipe(hash())
+        .pipe(gulp.dest("static/js"))
+})
+
+// Watch asset folder for changes
+gulp.task("watch", ["scss", "images", "js"], function () {
+    gulp.watch("src/scss/**/*", ["scss"])
+    gulp.watch("src/images/**/*", ["images"])
+    gulp.watch("src/js/**/*", ["js"])
+})
+
+// Set watch as default task
+gulp.task("default", ["watch"])
